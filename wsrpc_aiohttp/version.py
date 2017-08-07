6ab4d14933ca54e4bc796dcdb6b5252ff817edb2
Dmitry Orlov <me@mosquito.su>
2017-08-07 21:10:57 +0300

    [fix]

--- wsrpc_aiohttp/version.py
+++ wsrpc_aiohttp/version.py
@@ -9,7 +9,7 @@ package_license = "Apache Software License"
 
 team_email = 'me@mosquito.su'
 
-version_info = (0, 0, 2)
+version_info = (0, 1, 0)
 
 __author__ = ", ".join("{} <{}>".format(*info) for info in author_info)
 __version__ = ".".join(map(str, version_info))
