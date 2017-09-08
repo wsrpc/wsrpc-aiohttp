b568755f5516a285e2031d4209a581f953a6e130
Dmitry Orlov <me@mosquito.su>
2017-09-08 12:55:40 +0300

    [fix] bump

--- wsrpc_aiohttp/version.py
+++ wsrpc_aiohttp/version.py
@@ -9,7 +9,7 @@ package_license = "Apache Software License"
 
 team_email = 'me@mosquito.su'
 
-version_info = (0, 5, 0)
+version_info = (0, 5, 1)
 
 
 __author__ = ", ".join("{} <{}>".format(*info) for info in author_info)
