748f2c22a2c29ec3734e1f22b0827438fd02aac2
Nikitin Artyom <tzoiker@gmail.com>
2020-03-31 18:10:15 +0300

    Proxy explicitly in WebSocketRoute (#28)
    * feat: explicit proxy
    
    * test: explicit proxy
    
    * feat: keep compatibility
    
    * refactor
    
    * refactor
    
    Co-authored-by: Artyom Nikitin <a.nikitin@edadeal.ru>

--- wsrpc_aiohttp/websocket/route.py
+++ wsrpc_aiohttp/websocket/route.py
@@ -1,6 +1,8 @@
 import logging
 
 import asyncio
+from abc import ABC, abstractmethod
+
 from . import handler       # noqa
 
 log = logging.getLogger("wsrpc")
@@ -8,14 +10,62 @@ log = logging.getLogger("wsrpc")
 
 class decorators(object):
     _NOPROXY = set([])
+    _PROXY_ATTR = '__wsrpc_aiohttp_proxy__'
 
     @staticmethod
     def noproxy(func):
         decorators._NOPROXY.add(func)
         return func
 
+    @staticmethod
+    def proxy(f):
+        setattr(f, decorators._PROXY_ATTR, True)
+        return f
 
-class WebSocketRoute(object):
+    @staticmethod
+    def proxied(f):
+        return getattr(f, decorators._PROXY_ATTR, False)
+
+
+class AbstractRoute(ABC):
+
+    def __init__(self, obj: 'handler.WebSocketBase'):
+        self.socket = obj
+
+    @property
+    def loop(self) -> asyncio.AbstractEventLoop:
+        return self.socket._loop  # noqa
+
+    def _onclose(self):
+        pass
+
+    @abstractmethod
+    def proxy(self, method):
+        raise NotImplementedError
+
+    def _resolve(self, method):
+        if method.startswith('_'):
+            raise AttributeError('Trying to get private method.')
+
+        if hasattr(self, method) and self.proxy(method):
+            return getattr(self, method)
+        else:
+            raise NotImplementedError('Method not implemented')
+
+
+class Route(AbstractRoute):
+
+    @abstractmethod
+    def proxy(self, method):
+        func = getattr(self, method)
+        return decorators.proxied(func)
+
+    @decorators.proxy
+    def placebo(self, *args, **kwargs):
+        log.debug("PLACEBO IS CALLED!!! args: %r, kwargs: %r", args, kwargs)
+
+
+class LegacyRoute(Route):
     _NOPROXY = []
 
     @classmethod
@@ -28,32 +78,16 @@ class WebSocketRoute(object):
             return func(*args, **kwargs)
         return wrap
 
-    def __init__(self, obj: 'handler.WebSocketBase'):
-        self.socket = obj
-
-    @property
-    def loop(self) -> asyncio.AbstractEventLoop:
-        return self.socket._loop    # noqa
-
-    def _resolve(self, method):
-        if method.startswith('_'):
-            raise AttributeError('Trying to get private method.')
-
-        if hasattr(self, method):
-            func = getattr(self, method)
-            if func in decorators._NOPROXY:
-                raise NotImplementedError('Method not implemented')
-            else:
-                return func
-        else:
-            raise NotImplementedError('Method not implemented')
-
-    def _onclose(self):
-        pass
-
-    @classmethod
-    def placebo(*args, **kwargs):
-        log.debug("PLACEBO IS CALLED!!! args: %r, kwargs: %r", args, kwargs)
+    @abstractmethod
+    def proxy(self, method):
+        func = getattr(self, method)
+        return not (func in decorators._NOPROXY)
 
 
-__all__ = 'WebSocketRoute', 'decorators',
+WebSocketRoute = LegacyRoute
+
+
+__all__ = (
+    'AbstractRoute', 'Route',
+    'WebSocketRoute', 'LegacyRoute', 'decorators',
+)
