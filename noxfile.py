17c08ba56533bb314955127dd3e607b4cdd4588c
Ivan Sitkin <alvinera@yandex.ru>
2023-09-01 20:46:02 +0300

    feat: make nox great again

--- noxfile.py
+++ noxfile.py
@@ -1,7 +1,7 @@
-import subprocess
 from contextlib import chdir
 from pathlib import Path
 
+import nox
 import requests
 
 
@@ -13,26 +13,31 @@ def download(url: str, dest: Path):
         for chunk in response.iter_content(chunk_size=65535):
             fp.write(chunk)
 
-def docs():
+@nox.session(python=False)
+def tests(session: nox.Session):
+    session.run("poetry", "run", "pytest", "--asyncio-mode=auto", "-v", "tests")
+
+@nox.session(python=False)
+def docs(session: nox.Session):
     docs = Path("docs")
     plantuml = (Path("contrib") / "plantuml.jar").resolve()
 
     def plantuml_render(*filenames):
         with chdir(str(docs / "source")):
-            subprocess.run(
-                ["java", "-jar", str(plantuml), "-tsvg",
+            session.run(
+                "java", "-jar", str(plantuml), "-tsvg",
                 "-quiet", "-progress", "-overwrite", "-nometadata",
-                "-o", "_static", *filenames]
+                "-o", "_static", *filenames
             )
 
     plantuml_render("*.puml")
 
-    with chdir(str(docs)):
-        subprocess.run(["poetry", "run", "sphinx-build", "source", "build"])
+    with session.chdir(str(docs)):
+        session.run("poetry", "run", "sphinx-build", "source", "build")
 
 
-
-def build(*args, **kwargs):
+@nox.session(python=False)
+def build(session: nox.Session):
     base_url = "https://unpkg.com/@wsrpc/client"
     files = "wsrpc.d.ts", "wsrpc.es6.js"
     dist_files = "wsrpc.js", "wsrpc.min.js", "wsrpc.min.js.map"
@@ -44,3 +49,4 @@ def build(*args, **kwargs):
 
     for fname in dist_files:
         download(f'{base_url}/dist/{fname}', dest_path / fname)
+    session.run("poetry", "build")
